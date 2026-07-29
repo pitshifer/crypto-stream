9bcdc50a7681e9bfcc98fc430e7426c50e4cac6b
AntonK <pitshifer@gmail.com>
2026-07-29 21:06:20 +0700

    Refactor main function to handle multiple trading symbols concurrently

--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,42 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/pitshifer/crypto-stream/internal/binance"
 )
 
 func main() {
-	client := binance.NewClient("wss://stream.binance.com:9443/ws/btcusdt@trade")
-	if err := client.Connect(); err != nil {
-		log.Fatal("connection error:", err)
-	}
-	defer client.Close()
-
+	symbols := []string{"btcusdt", "ethusdt", "bnbusdt"}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	events, err := client.Stream(ctx)
-	if err != nil {
-		log.Fatal("stream error:", err)
+
+	wg := sync.WaitGroup{}
+	wg.Add(len(symbols))
+
+	for _, symbol := range symbols {
+		go func(s string) {
+			defer wg.Done()
+
+			client := binance.NewClient("wss://stream.binance.com:9443/ws/" + s + "@trade")
+			if err := client.Connect(); err != nil {
+				log.Println("connection error:", err)
+				return
+			}
+			defer client.Close()
+
+			events, err := client.Stream(ctx)
+			if err != nil {
+				log.Println("stream error:", err)
+				return
+			}
+
+			for event := range events {
+				log.Printf("Trade Event: %+v\n", event)
+			}
+		}(symbol)
 	}
 
-	for event := range events {
-		log.Printf("Trade Event: %+v\n", event)
-	}
+	wg.Wait()
 }
